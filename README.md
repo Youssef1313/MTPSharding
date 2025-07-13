54c3a5ffb84ea9690cc96e89b867b7c274ea2acf
Youssef1313 <youssefvictor00@gmail.com>
2025-07-13 12:09:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MTPBatching
+
+[![YTest.MTP.Batching NuGet Badge](https://img.shields.io/nuget/v/YTest.MTP.Batching?label=Latest&logo=nuget)](https://www.nuget.org/packages/YTest.MTP.Batching/)
---
+++ samples/MTPBatchingSample/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Testing.Platform.Builder;
+using MTPBatchingSample.MSTest;
+
+ITestApplicationBuilder builder = await TestApplication.CreateBuilderAsync(args);
+SelfRegisteredExtensions.AddSelfRegisteredExtensions(builder, args);
+using (ITestApplication app = await builder.BuildAsync())
+{
+    return await app.RunAsync();
+}
