6097ebf4f7d3ee92225b85da5e8fdce3bd8183fb
Youssef1313 <youssefvictor00@gmail.com>
2025-07-13 17:10:47 +0200

    Try out

--- src/YTest.MTP.Batching.Console/Program.cs
+++ src/YTest.MTP.Batching.Console/Program.cs
@@ -1,3 +1,9 @@
 ﻿using System;
+using YTest.MTP.PipeProtocol;
 
-Console.WriteLine("Hello, World!");
+var discoverer = new MTPPipeDiscoverer("C:\\Users\\ygerges\\Desktop\\MTPBatching\\artifacts\\bin\\YTest.MTP.Batching.Tests\\debug\\YTest.MTP.Batching.Tests.exe", "");
+var tests = await discoverer.DiscoverTests();
+foreach (var test in tests)
+{
+    Console.WriteLine(test.Uid + ", " + test.DisplayName);
+}
